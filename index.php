ffa0fe6a57c7e82fd89d209e91188cf59a912695
Pierre-louis Sans <pierre.louis.sans@gmail.com>
2022-12-12 11:19:54 +0100

    footer

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php require_once "template/header.php"?>
+Hello world
+<?php require_once "template/footer.php"?>
--- template/footer.php
+++ template/footer.php
@@ -1,1 +1,50 @@
-<?php
+<footer id="colophon" class="site-footer">
+  <style>
+
+  </style>
+  <section class="FS FS1">
+    <p class="FDP" >Ecologica est l’école supérieure de la transition résiliente, solidaire et désirable, à la fois lieu d’apprentissages et de participations à un changement de modèle de société.</p>
+    <div>
+      <h3>Navigation</h3>
+      <ul>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">L'école</a></li>
+        <li><a href="#">Les formations</a></li>
+        <li><a href="#">Candidater</a></li>
+        <li><a href="#">Constacte</a></li>
+      </ul>
+    </div>
+    <div>
+      <h3>Ecologica</h3>
+      <ul>
+        <li><a href="#">L’école</a></li>
+        <li><a href="#">La gouvernance</a></li>
+        <li><a href="#">Admissions et Tarifs</a></li>
+        <li><a href="#">La vie étudiante</a></li>
+        <li><a href="#">Le réseaux Ecologica</a></li>
+      </ul>
+    </div>
+    <div>
+      <h3>Nous contacter</h3>
+      <p>Téléphone: +33 6 17 20 44 92</p>
+      <p>Email: contact@ecologica.education</p>
+    </div>
+  </section>
+  <section class="FS FS2">
+    <p >Copyright © 2022 <br> IIM - Institue de l'internet et des multimedia</p>
+    <ul>
+      <li><a href="#"><ion-icon name="logo-youtube"></ion-icon></a></li>
+      <li><a href="#"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+      <li><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
+      <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
+    </ul>
+    <p>Politique de confidentialité – Mentions légales</p>
+  </section>
+</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+</body>
+</html>
