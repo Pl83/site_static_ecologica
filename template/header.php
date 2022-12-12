15b9cda3de6b85f20880d8510a578dc536cc6ec4
Thea Blachon <thea.blachon73@gmail.com>
2022-12-12 11:22:45 +0100

    ok

--- template/header.php
+++ template/header.php
@@ -1,1 +1,24 @@
-<?php
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="ecologica-menu">
+    <div class="logo-menu">
+        <img src="../img/logo.png" alt="logo">
+        <a href="index.php">Accueil</a>
+        <a href="admissions_formations.php">Admissions & formations</a>
+        <a href="ecologica.php">Ecologica</a>
+        <a href="partenaires.php">Partenaires</a>
+        <a href="contact.php">Contact</a>
+    </div>
+    <div class="candidater">
+        <a href="">Candidatez</a>
+    </div>
+</div>
