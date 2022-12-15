ebbd8ab29313b6cdf8f88f9f461add0d8e566045
Pierre-louis Sans <pierre.louis.sans@gmail.com>
2022-12-15 13:30:50 +0100

    fixing some merge pb

--- template/header.php
+++ template/header.php
@@ -15,12 +15,6 @@
     <div class="logo-menu">
         <img src="../img/logo_blanc.svg" alt="logo">
 
-        <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg%22%3E">
-        <path d="M1 6L14 6" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
-        <path d="M1 12L14 12" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
-        <path d="M1 18L14 18" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
-        </svg>
-
         <ul>
             <a href="index.php">Accueil</a>
             <a href="admissions_formations.php">Admissions & formations</a>
