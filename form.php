ea504b9c48ec379cca44b8c2d8ab6a7ea774b90b
Pierre-louis Sans <pierre.louis.sans@gmail.com>
2023-01-04 21:11:02 +0100

    delete a > that was broking the js

--- form.php
+++ form.php
@@ -143,7 +143,7 @@
         tabcontent = document.getElementsByClassName("tabcontent");
         for (i = 0; i < tabcontent.length; i++) {
             tabcontent[i].style.display = "none";
-        }<
+        }
         tablinks = document.getElementsByClassName("tablinks");
         for (i = 0; i < tablinks.length; i++) {
             tablinks[i].className = tablinks[i].className.replace(" active", "");
